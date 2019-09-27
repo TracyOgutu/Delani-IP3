72a96a5a287ff8aedfc0dcd514189699586a6e84
Tracy Ogutu <tracyogutu@gmail.com>
2019-09-27 15:46:53 +0300

    add javascript function

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function userDetail(){
+
+    var name=parseInt(document.getElementById("name"));
+    var email=parseInt(document.getElementById("email"));
+    var message=parseInt(document.getElementById("message"));
+
+    alert(name);
+    alert(email);
+    alert(message);
+}
